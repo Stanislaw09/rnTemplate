24791a8167a833983e977db6c87d7757e1c63c04
Stanislaw09 <sgardzielewski09@gmail.com>
2024-03-07 13:39:19 +0100

    conditional navigation added

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { DrawerScreenProps, createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import {
+   NativeStackScreenProps,
+   createNativeStackNavigator,
+} from '@react-navigation/native-stack';
+import { useState } from 'react';
 
 function HomeScreen({ navigation }: DrawerScreenProps<any>) {
    return (
@@ -22,15 +27,44 @@ function NotificationsScreen({ navigation }: DrawerScreenProps<any>) {
    );
 }
 
+function LoginScreen({ navigation }: NativeStackScreenProps<any>) {
+   return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         <Text>Login Screen</Text>
+         <Button onPress={() => navigation.navigate('signup')} title="Go to sign up" />
+      </View>
+   );
+}
+function SignUpScreen({ navigation }: NativeStackScreenProps<any>) {
+   return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         <Text>Sign up Screen</Text>
+         <Button onPress={() => navigation.navigate('login')} title="Go to login" />
+      </View>
+   );
+}
+
 const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
 
 function App() {
+   const [auth, setAuth] = useState(false);
+
    return (
       <NavigationContainer>
-         <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen name="Home" component={HomeScreen} />
-            <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-         </Drawer.Navigator>
+         {auth ? (
+            <Stack.Navigator>
+               <Stack.Screen name="login" component={LoginScreen} />
+               <Stack.Screen name="signup" component={SignUpScreen} />
+            </Stack.Navigator>
+         ) : (
+            <Drawer.Navigator initialRouteName="Home">
+               <Drawer.Screen name="Home" component={HomeScreen} />
+               <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+            </Drawer.Navigator>
+         )}
+
+         <Button title='switch auth' onPress={() => setAuth(!auth)} />
       </NavigationContainer>
    );
 }
